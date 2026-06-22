54d66c0d242252d40dac02cc0065ffbeb063ad6c
drodv13 <111702958+drodv13@users.noreply.github.com>
2026-06-21 23:52:22 -0500

    Update query_tests.py

--- tests/query_tests.py
+++ tests/query_tests.py
@@ -1,30 +1,13 @@
 from app_functions.connect_db import connect_db
-from agent_functions.run_pipeline import run_pipeline
+from agent_functions.run_query import run_query
 import pandas as pd
 
 
-db = connect_db("proyectoBDA")
-coleccion = db["leyes"]
+db = connect_db("sample_mflix")
+movies = "movies"
+filter = {"title": "The Great Train Robbery"}
+limit = 5
 
 
-print(coleccion.distinct("norma"))
-
-"""
-What are the top 5 contracts with the highest monto_contratado_total in the technology sector? Just consider the moneda: Dólar Norteamericano. Show me the descripcion_proceso, moneda and monto_contratado_total
-"""
-
-
-"""
-pipeline = [{'$match': {'clasificacion.sector': 'Tecnología'}}, 
-            {'$sort': {'monto_contratado_total': -1}}, 
-            {'$limit': 5}
-            ]
-
-
-resultado = run_pipeline(db, contrataciones, pipeline)
-
-if isinstance(resultado, list):
-    print(pd.DataFrame(resultado))
-else:
-    print(resultado)
-"""
+resultado = run_query(db, movies, filter, limit)
+print(pd.DataFrame(resultado))
