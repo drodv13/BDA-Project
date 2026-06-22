0714e583645911c46432d7ae305b3029de356638
drodv13 <111702958+drodv13@users.noreply.github.com>
2026-06-21 23:43:10 -0500

    Update connect_db.py

--- app_functions/connect_db.py
+++ app_functions/connect_db.py
@@ -1,60 +1,23 @@
-from google.genai import types
-from agent_functions.run_query import schema_run_query, run_query
-from agent_functions.run_pipeline import schema_run_pipeline, run_pipeline
-from agent_functions.read_contratacion import schema_read_contratacion, read_contratacion
-from agent_functions.search_related_leyes import schema_search_related_leyes, search_related_leyes
-from collections.abc import Callable
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
 
 
-def call_function( database, function_call: types.FunctionCall, verbose: bool = False ) -> types.Content:
-    if verbose:
-        print(f"Calling function: {function_call.name}({function_call.args})")
-    else:
-        print(f" - Calling function: {function_call.name}")
+def connect_db(database: str):
+    print("Connecting to the Mongo database...")
 
-    function_map: dict[str, Callable[..., str]] = {
-                    "run_query": run_query,
-                    "run_pipeline": run_pipeline,
-                    "read_contratacion": read_contratacion,
-                    "search_related_leyes": search_related_leyes,
-                    }
-    
-    if function_call.name in function_map:
-        args = dict(function_call.args) if function_call.args else {}
+    load_dotenv()
 
-        if function_call.name == "read_contratacion":
-            function_result = function_map[function_call.name](**args)
-        else:
-            args["database"] = database
-            function_result = function_map[function_call.name](**args)
+    connection_string = os.getenv("MONGO_CONNECTION_STRING")
 
-        return types.Content(
-            role="tool",
-            parts=[
-                types.Part.from_function_response(
-                    name=function_call.name,
-                    response={"result": function_result},
-                )
-            ],
-        )
+    if not connection_string: raise RuntimeError("The Mongo Connection String was not found.")
 
-    return types.Content(
-        role="tool",
-        parts=[
-            types.Part.from_function_response(
-                name=function_call.name,
-                response={"error": f"Unknown function: {function_call.name}"},
-            )
-        ],
-    )
+    client = MongoClient(connection_string)
 
+    if not client: raise RuntimeError("Client is None.")
 
-    
+    db = client[database]
 
-available_functions = types.Tool(
-    function_declarations=[schema_run_query,
-                           schema_run_pipeline,
-                           schema_read_contratacion,
-                           schema_search_related_leyes,
-                           ],
-)
+    print("Connected to the Mongo database")
+
+    return db
