d371996e8443386174fbb40713d47cda6621d091
drodv13 <111702958+drodv13@users.noreply.github.com>
2026-06-21 23:44:57 -0500

    Update print_tokens.py

--- app_functions/print_tokens.py
+++ app_functions/print_tokens.py
@@ -1,2 +1,12 @@
-def generate_model_config():
-    return None
+from google import genai
+
+def print_tokens(response):
+    metadata = response.usage_metadata
+    if not metadata: raise Exception("No metadata was found.")
+
+    prompt_tokens   = metadata.prompt_token_count
+    response_tokens = metadata.candidates_token_count
+
+    if not prompt_tokens or not response_tokens: raise Exception("Token-usage data is not complete.")
+
+    print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}")
